73344c3b5413e8c1bf797674e93cb84a321e7ca3
MiguelReaG <miguel.rea6871@alumnos.udg.mx>
2025-04-28 03:50:17 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -99,7 +99,7 @@ document.querySelector('.sign-in-container form').addEventListener('submit', asy
     }
 
     try {
-        const response = await sendRequest('http://localhost:8000/auth/login', 'POST', data);
+        const response = await sendRequest('https://auth-api-v2-398o.onrender.com/auth/login', 'POST', data);
 
         if (response.access_token) {
             localStorage.setItem('access_token', response.access_token); // Guardar el token
