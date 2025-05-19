f257d749c0cc77e0dd7b5a28e397a8176dc79394
Uriel Benitez <143746247+UrielOrlandoBenitezTorres@users.noreply.github.com>
2025-05-19 14:00:52 -0600

    Delete BaldRat directory

--- BaldRat/DB_Connect/create_db.py
+++
@@ -1,6 +0,0 @@
-from BaldRat.DB_Connect.database import Base, engine
-from BaldRat.DB_Connect.models import Project, ProjectCollaborator
-
-# Crear las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
-print("✅ Tablas creadas exitosamente.")
